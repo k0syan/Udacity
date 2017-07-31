52b1c705b68c47215bac4f5904e7170d70b8786c
shahenkosyan <shahen.qosyan@gmail.com>
2017-07-31 13:00:00 +0400

    T01.02-Exercise-DisplayToyList - done

--- Developing_Android_Apps/FavoriteToys/app/src/main/java/com/example/favoritetoys/MainActivity.java
+++ Developing_Android_Apps/FavoriteToys/app/src/main/java/com/example/favoritetoys/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.favoritetoys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	TextView mToysListTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mToysListTextView = findViewById(R.id.tv_toy_names);
+
+		String[] toys = ToyBox.getToyNames();
+
+		for (String toy : toys) {
+			mToysListTextView.append(toy + "\n\n\n");
+		}
+	}
 }

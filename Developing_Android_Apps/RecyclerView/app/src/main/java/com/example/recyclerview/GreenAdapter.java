3812e9ceac5aa68c436bb107224277529215979e
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-03 12:52:14 +0400

    Update project

--- Developing_Android_Apps/RecyclerView/app/src/main/java/com/example/recyclerview/GreenAdapter.java
+++ Developing_Android_Apps/RecyclerView/app/src/main/java/com/example/recyclerview/GreenAdapter.java
@@ -2,6 +2,7 @@ package com.example.recyclerview;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,22 @@ import com.example.android.recyclerview.R;
 
 public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
 
+	private static final String TAG = GreenAdapter.class.getSimpleName();
+
+	// TODO (3) Create a final private ListItemClickListener called mOnClickListener
+
+	private static int viewHolderCount;
+
 	private int mNumberItems;
+
+	// TODO (1) Add an interface called ListItemClickListener
+	// TODO (2) Within that interface, define a void method called onListItemClick that takes an int as a parameter
+
+	// TODO (4) Add a ListItemClickListener as a parameter to the constructor and store it in mOnClickListener
 
 	public GreenAdapter(int numberOfItems) {
 		mNumberItems = numberOfItems;
+		viewHolderCount = 0;
 	}
 
 	@Override
@@ -21,14 +34,26 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 		Context context = viewGroup.getContext();
 		int layoutIdForListItem = R.layout.number_list_item;
 		LayoutInflater inflater = LayoutInflater.from(context);
+		boolean shouldAttachToParentImmediately = false;
 
-		View view = inflater.inflate(layoutIdForListItem, viewGroup, false);
+		View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+		NumberViewHolder viewHolder = new NumberViewHolder(view);
 
-		return new NumberViewHolder(view);
+		viewHolder.viewHolderIndex.setText("ViewHolder index: " + viewHolderCount);
+
+		int backgroundColorForViewHolder = ColorUtils
+				.getViewHolderBackgroundColorFromInstance(context, viewHolderCount);
+		viewHolder.itemView.setBackgroundColor(backgroundColorForViewHolder);
+
+		viewHolderCount++;
+		Log.d(TAG, "onCreateViewHolder: number of ViewHolders created: "
+				+ viewHolderCount);
+		return viewHolder;
 	}
 
 	@Override
 	public void onBindViewHolder(NumberViewHolder holder, int position) {
+		Log.d(TAG, "#" + position);
 		holder.bind(position);
 	}
 
@@ -37,18 +62,24 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 		return mNumberItems;
 	}
 
+	// TODO (5) Implement OnClickListener in the NumberViewHolder class
 	class NumberViewHolder extends RecyclerView.ViewHolder {
 
 		TextView listItemNumberView;
+		TextView viewHolderIndex;
 
-		private NumberViewHolder(View itemView) {
+		public NumberViewHolder(View itemView) {
 			super(itemView);
 
-			listItemNumberView = itemView.findViewById(R.id.tv_item_number);
+			listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+			viewHolderIndex = (TextView) itemView.findViewById(R.id.tv_view_holder_instance);
+			// TODO (7) Call setOnClickListener on the View passed into the constructor (use 'this' as the OnClickListener)
 		}
 
 		void bind(int listIndex) {
 			listItemNumberView.setText(String.valueOf(listIndex));
 		}
+
+		// TODO (6) Override onClick, passing the clicked item's position (getAdapterPosition()) to mOnClickListener via its onListItemClick method
 	}
 }
--- Developing_Android_Apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ Developing_Android_Apps/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -4,28 +4,60 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import com.example.android.recyclerview.R;
 
+// TODO (8) Implement GreenAdapter.ListItemClickListener from the MainActivity
 public class MainActivity extends AppCompatActivity {
 
-	private static final int NUM_LIST_ITEMS = 50;
+	private static final int NUM_LIST_ITEMS = 100;
 
 	private GreenAdapter mAdapter;
 	private RecyclerView mNumbersList;
+
+	// TODO (9) Create a Toast variable called mToast to store the current Toast
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mNumbersList = findViewById(R.id.rv_numbers);
+		mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
 
 		LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 		mNumbersList.setLayoutManager(layoutManager);
+
 		mNumbersList.setHasFixedSize(true);
 
+		// TODO (13) Pass in this as the ListItemClickListener to the GreenAdapter constructor
 		mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
-
 		mNumbersList.setAdapter(mAdapter);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		int itemId = item.getItemId();
+
+		switch (itemId) {
+			case R.id.action_refresh:
+				// TODO (14) Pass in this as the ListItemClickListener to the GreenAdapter constructor
+				mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+				mNumbersList.setAdapter(mAdapter);
+				return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	// TODO (10) Override ListItemClickListener's onListItemClick method
+	// TODO (11) In the beginning of the method, cancel the Toast if it isn't null
+	// TODO (12) Show a Toast when an item is clicked, displaying that item number that was clicked
 }

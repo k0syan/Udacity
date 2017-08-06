5f7d585f34b1e92ece3e4b429c544fdd56236ac2
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-06 21:47:53 +0400

    T05a.02-Exercise-PersistData

--- Developing_Android_Apps/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
+++ Developing_Android_Apps/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
 
+	// TODO (1) Create a key String called LIFECYCLE_CALLBACKS_TEXT_KEY
+
 	private static final String ON_CREATE = "onCreate";
 	private static final String ON_START = "onStart";
 	private static final String ON_RESUME = "onResume";
@@ -28,44 +30,58 @@ public class MainActivity extends AppCompatActivity {
 
 		mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
+		// TODO (6) If savedInstanceState is not null and contains LIFECYCLE_CALLBACKS_TEXT_KEY, set that text on our TextView
+
 		logAndAppend(ON_CREATE);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
+
 		logAndAppend(ON_START);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+
 		logAndAppend(ON_RESUME);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
+
 		logAndAppend(ON_PAUSE);
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
+
 		logAndAppend(ON_STOP);
 	}
 
 	@Override
 	protected void onRestart() {
 		super.onRestart();
+
 		logAndAppend(ON_RESTART);
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
+
 		logAndAppend(ON_DESTROY);
 	}
+
+	// TODO (2) Override onSaveInstanceState
+	// Do steps 3 - 5 within onSaveInstanceState
+	// TODO (3) Call super.onSaveInstanceState
+	// TODO (4) Call logAndAppend with the ON_SAVE_INSTANCE_STATE String
+	// TODO (5) Put the text from the TextView in the outState bundle
 
 	private void logAndAppend(String lifecycleEvent) {
 		Log.d(TAG, "Lifecycle Event: " + lifecycleEvent);

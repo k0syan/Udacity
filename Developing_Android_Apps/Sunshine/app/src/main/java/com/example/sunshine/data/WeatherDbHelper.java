e6d884cbbda83a820834fc8cabac0803c451e86a
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-09 17:34:09 +0400

    S07.03-Exercise-ConflictResolutionPolicy - done

--- Developing_Android_Apps/Sunshine/app/src/main/java/com/example/sunshine/data/WeatherDbHelper.java
+++ Developing_Android_Apps/Sunshine/app/src/main/java/com/example/sunshine/data/WeatherDbHelper.java
@@ -9,8 +9,8 @@ import com.example.sunshine.data.WeatherContract.WeatherEntry;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
 	public static final String DATABASE_NAME = "weather.db";
-	//  TODO (2) Increment the database version after changing the create table statement
-	private static final int DATABASE_VERSION = 2;
+
+	private static final int DATABASE_VERSION = 3;
 
 	public WeatherDbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,7 +22,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 		final String SQL_CREATE_WEATHER_TABLE =
 				"CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
 						WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-						WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+						WeatherEntry.COLUMN_DATE + " INTEGER UNIQUE NOT NULL, " +
 						WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
 						WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
@@ -30,7 +30,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 						WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL" + ");";
-//              TODO (1) Add a UNIQUE constraint on the date column to replace on conflict
+
 		sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
 	}
 

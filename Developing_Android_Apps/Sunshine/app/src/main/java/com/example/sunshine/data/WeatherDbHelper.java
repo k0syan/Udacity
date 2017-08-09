2461e2c07b579b2f366d7e28e9d5a9a6d6120cf9
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-09 17:37:44 +0400

    Update Db Helper

--- Developing_Android_Apps/Sunshine/app/src/main/java/com/example/sunshine/data/WeatherDbHelper.java
+++ Developing_Android_Apps/Sunshine/app/src/main/java/com/example/sunshine/data/WeatherDbHelper.java
@@ -22,14 +22,15 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 		final String SQL_CREATE_WEATHER_TABLE =
 				"CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
 						WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-						WeatherEntry.COLUMN_DATE + " INTEGER UNIQUE NOT NULL, " +
+						WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
 						WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
 						WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
 						WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-						WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL" + ");";
+						WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL" +
+						" UNIQUE (" + WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE);";
 
 		sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
 	}

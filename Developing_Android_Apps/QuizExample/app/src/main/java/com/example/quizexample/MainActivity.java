3fffadf24f90625815db42bfaadef55384eb063d
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-10 16:40:10 +0400

    T08.02-Exercise-AddAsyncTaskToRetrieveCursor - done

--- Developing_Android_Apps/QuizExample/app/src/main/java/com/example/quizexample/MainActivity.java
+++ Developing_Android_Apps/QuizExample/app/src/main/java/com/example/quizexample/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.quizexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 public class MainActivity extends AppCompatActivity {
+
+	private int mCurrentState;
+
+	private Cursor mData;
+
+	private Button mButton;
+
+	private final int STATE_HIDDEN = 0;
+
+	private final int STATE_SHOWN = 1;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mButton = (Button) findViewById(R.id.button_next);
+
+		new WordFetchTask().execute();
+	}
+
+	public void onButtonClick(View view) {
+
+		switch (mCurrentState) {
+			case STATE_HIDDEN:
+				showDefinition();
+				break;
+			case STATE_SHOWN:
+				nextWord();
+				break;
+		}
+	}
+
+	public void nextWord() {
+		mButton.setText(getString(R.string.show_definition));
+
+		mCurrentState = STATE_HIDDEN;
+	}
+
+	public void showDefinition() {
+		mButton.setText(getString(R.string.next_word));
+
+		mCurrentState = STATE_SHOWN;
+	}
+
+	public class WordFetchTask extends AsyncTask<Void, Void, Cursor> {
+
+		@Override
+		protected Cursor doInBackground(Void... params) {
+			ContentResolver resolver = getContentResolver();
+			Cursor cursor = resolver.query(DroidTermsExampleContract.CONTENT_URI,
+					null, null, null, null);
+			return cursor;
+		}
+
+		@Override
+		protected void onPostExecute(Cursor cursor) {
+			super.onPostExecute(cursor);
+			mData = cursor;
+		}
 	}
 }

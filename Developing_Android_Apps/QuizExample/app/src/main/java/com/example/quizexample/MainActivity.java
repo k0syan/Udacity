db4f85f58bceda2fea55240d5d640ea3a1f46d4b
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-10 16:54:00 +0400

    T08.03-Exercise-FinishQuizExample - done

--- Developing_Android_Apps/QuizExample/app/src/main/java/com/example/quizexample/MainActivity.java
+++ Developing_Android_Apps/QuizExample/app/src/main/java/com/example/quizexample/MainActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import android.widget.TextView;
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 public class MainActivity extends AppCompatActivity {
 
+	private Cursor mData;
+
 	private int mCurrentState;
 
-	private Cursor mData;
+	private int mDefCol, mWordCol;
+
+	private TextView mWordTextView, mDefinitionTextView;
 
 	private Button mButton;
 
@@ -27,9 +33,13 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mWordTextView = (TextView) findViewById(R.id.text_view_word);
+		mDefinitionTextView = (TextView) findViewById(R.id.text_view_definition);
+
 		mButton = (Button) findViewById(R.id.button_next);
 
 		new WordFetchTask().execute();
+
 	}
 
 	public void onButtonClick(View view) {
@@ -47,13 +57,34 @@ public class MainActivity extends AppCompatActivity {
 	public void nextWord() {
 		mButton.setText(getString(R.string.show_definition));
 
-		mCurrentState = STATE_HIDDEN;
+		if (mData != null) {
+			if (!mData.moveToNext()) {
+				mData.moveToFirst();
+			}
+			mDefinitionTextView.setVisibility(View.INVISIBLE);
+			mButton.setText(getString(R.string.show_definition));
+			mWordTextView.setText(mData.getString(mWordCol));
+			mDefinitionTextView.setText(mData.getString(mDefCol));
+			mCurrentState = STATE_HIDDEN;
+		}
 	}
 
 	public void showDefinition() {
 		mButton.setText(getString(R.string.next_word));
 
-		mCurrentState = STATE_SHOWN;
+		if (mData != null) {
+			mDefinitionTextView.setVisibility(View.VISIBLE);
+			mButton.setText(getString(R.string.next_word));
+			mCurrentState = STATE_SHOWN;
+		}
+
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mData.close();
 	}
 
 	public class WordFetchTask extends AsyncTask<Void, Void, Cursor> {
@@ -61,15 +92,24 @@ public class MainActivity extends AppCompatActivity {
 		@Override
 		protected Cursor doInBackground(Void... params) {
 			ContentResolver resolver = getContentResolver();
+
 			Cursor cursor = resolver.query(DroidTermsExampleContract.CONTENT_URI,
 					null, null, null, null);
 			return cursor;
 		}
 
+
 		@Override
 		protected void onPostExecute(Cursor cursor) {
 			super.onPostExecute(cursor);
+
 			mData = cursor;
+
+			mDefCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+			mWordCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+
+			nextWord();
 		}
 	}
+
 }

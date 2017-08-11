8df5cab94c2fd2f877a72bfc5c266622b1c7d648
shahenkosyan <shahen.qosyan@gmail.com>
2017-08-11 15:20:20 +0400

    T10.01-Exercise-IntentServices - done

--- Developing_Android_Apps/HydrationReminder/app/src/main/java/com/example/hydrationreminder/MainActivity.java
+++ Developing_Android_Apps/HydrationReminder/app/src/main/java/com/example/hydrationreminder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hydrationreminder;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.example.hydrationreminder.sync.ReminderTasks;
+import com.example.hydrationreminder.sync.WaterReminderIntentService;
 import com.example.hydrationreminder.utilities.PreferenceUtilities;
 
 public class MainActivity extends AppCompatActivity implements
@@ -51,6 +54,10 @@ public class MainActivity extends AppCompatActivity implements
 		if (mToast != null) mToast.cancel();
 		mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
 		mToast.show();
+
+		Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
+		incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
+		startService(incrementWaterCountIntent);
 	}
 
 	@Override
--- Developing_Android_Apps/HydrationReminder/app/src/main/java/com/example/hydrationreminder/sync/ReminderTasks.java
+++ Developing_Android_Apps/HydrationReminder/app/src/main/java/com/example/hydrationreminder/sync/ReminderTasks.java
@@ -1,1 +1,19 @@
 package com.example.hydrationreminder.sync;
+
+import android.content.Context;
+import com.example.hydrationreminder.utilities.PreferenceUtilities;
+
+public class ReminderTasks {
+
+	public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+	private static void incrementWaterCount(Context context) {
+		PreferenceUtilities.incrementWaterCount(context);
+	}
+
+	public static void executeTask(Context context, String action) {
+		if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
+			incrementWaterCount(context);
+		}
+	}
+}
